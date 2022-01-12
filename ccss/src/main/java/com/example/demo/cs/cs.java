c7faa6a92b79abfdd89925af613ae8343e0ddfbb
sanyuan <2773503370@qq.com>
2022-01-12 09:37:45 +0800

    hot-fix commit01

--- ccss/src/main/java/com/example/demo/cs/cs.java
+++ ccss/src/main/java/com/example/demo/cs/cs.java
@@ -3,5 +3,6 @@ package com.example.demo.cs;
 public class cs {
     public static void main(String[] args) {
         System.out.println("第一行代码");
+        System.out.println("这是hot-fix01");
     }
 }
